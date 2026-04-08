ce9f635add3546d0926ccfe68b62b8a21d1c8e15
wei-zuan <269530115+wei-zuan@users.noreply.github.com>
2026-04-08 08:09:39 +0000

    feat: add ShieldToken as non-transferable qToken (ERC-20)

---
+++ contracts/ShieldToken.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ShieldToken is ERC20 {
+    address public vault;
+
+    modifier onlyVault() {
+        require(msg.sender == vault, "Only QRYPTANK can call this");
+        _;
+    }
+
+    constructor(string memory name, string memory symbol, address _vault) ERC20(name, symbol) {
+        vault = _vault;
+    }
+
+    function mint(address to, uint256 amount) external onlyVault {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyVault {
+        _burn(from, amount);
+    }
+
+    function transfer(address, uint256) public pure override returns (bool) {
+        revert("qToken: transfers disabled, use Qryptum app");
+    }
+
+    function transferFrom(address, address, uint256) public pure override returns (bool) {
+        revert("qToken: transfers disabled, use Qryptum app");
+    }
+
+    function approve(address, uint256) public pure override returns (bool) {
+        revert("qToken: approvals disabled");
+    }
+}
