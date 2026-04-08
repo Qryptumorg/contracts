d9522a49d3891a1e8ec68cb00e7cb6a4de9f8d87
wei-zuan <269530115+wei-zuan@users.noreply.github.com>
2026-04-08 20:14:26 +0000

    feat: add contracts/ShieldToken.sol

--- contracts/ShieldToken.sol
+++ contracts/ShieldToken.sol
@@ -5,14 +5,20 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ShieldToken is ERC20 {
     address public vault;
+    uint8 private _decimals;
 
     modifier onlyVault() {
         require(msg.sender == vault, "Only QRYPTANK can call this");
         _;
     }
 
-    constructor(string memory name, string memory symbol, address _vault) ERC20(name, symbol) {
+    constructor(string memory name, string memory symbol, address _vault, uint8 decimals_) ERC20(name, symbol) {
         vault = _vault;
+        _decimals = decimals_;
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
     }
 
     function mint(address to, uint256 amount) external onlyVault {
