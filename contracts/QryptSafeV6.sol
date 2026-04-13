cbb6ff8d01e9ae4d208832aa4b25e5f1ff061a9d
wei-zuan <wei-zuan@users.noreply.github.com>
2026-04-13 05:04:05 +0000

    fix(contracts): rename createVault/hasVault/getVault to createQryptSafe/hasQryptSafe/getQryptSafe (V6 factory)

--- contracts/QryptSafeV6.sol
+++ contracts/QryptSafeV6.sol
@@ -1,5 +1,30 @@
 // SPDX-License-Identifier: MIT
-// Qryptum Protocol v6.0 -- https://qryptum.org
+/*
+ *
+ *         ███████████████████████████████████████
+ *         ███                                 ███
+ *         ███                                 ███
+ * ███████████████████████████████████████████████████████
+ * ███████████████████████████████████████████████████████
+ * ███                                                 ███
+ * ███     ███  ████  █   █ ████  █████ █   █ █   █    ███
+ * ███    █   █ █   █ █   █ █   █   █   █   █ ██ ██    ███
+ * ███    █   █ ████   █ █  ████    █   █   █ █ █ █    ███
+ * ███    █  ██ █ █     █   █       █   █   █ █   █    ███
+ * ███     ██ █ █  █    █   █       █    ███  █   █    ███
+ * ███                                                 ███
+ * ███                      ████                       ███
+ * ███                     ██  ██                      ███
+ * ███                     ██  ██                      ███
+ * ███                      ████                       ███
+ * ███                       ██                        ███
+ * ███                       ██                        ███
+ * ███                                                 ███
+ * ███████████████████████████████████████████████████████
+ * ███████████████████████████████████████████████████████
+ *
+ */
+// https://qryptum.org
 pragma solidity 0.8.34;
 
 import "@openzeppelin/contracts/proxy/Clones.sol";
@@ -8,32 +33,34 @@ import "./PersonalQryptSafeV6.sol";
 contract QryptSafeV6 {
     using Clones for address;
 
-    address public immutable vaultImplementation;
+    bytes32 private constant _QRYPTUM_SALT = keccak256("qryptum.v6.sepolia");
+
+    address public immutable qryptSafeImpl;
     mapping(address => address) private vaults;
 
-    event VaultCreated(address indexed owner, address indexed vault);
+    event QryptSafeCreated(address indexed owner, address indexed vault);
 
     constructor() {
-        vaultImplementation = address(new PersonalQryptSafeV6());
+        qryptSafeImpl = address(new PersonalQryptSafeV6());
     }
 
     // initialChainHead = H100 of OTP chain, computed by frontend from vault proof
-    function createVault(bytes32 initialChainHead) external returns (address vault) {
+    function createQryptSafe(bytes32 initialChainHead) external returns (address vault) {
         require(vaults[msg.sender] == address(0), "Qrypt-Safe already exists for this wallet");
         require(initialChainHead != bytes32(0), "Invalid chain head");
 
-        vault = vaultImplementation.clone();
+        vault = qryptSafeImpl.clone();
         PersonalQryptSafeV6(vault).initialize(msg.sender, initialChainHead);
         vaults[msg.sender] = vault;
 
-        emit VaultCreated(msg.sender, vault);
+        emit QryptSafeCreated(msg.sender, vault);
     }
 
-    function hasVault(address wallet) external view returns (bool) {
+    function hasQryptSafe(address wallet) external view returns (bool) {
         return vaults[wallet] != address(0);
     }
 
-    function getVault(address wallet) external view returns (address) {
+    function getQryptSafe(address wallet) external view returns (address) {
         return vaults[wallet];
     }
 }
